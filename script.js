2b2c6c59107c419f65c1d7f69547b71a8c2e2775
Menos  Velasco jr <menozvelasco1@gmail.com>
2021-02-25 15:12:08 -0800

    adding handling click Event check input number

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 'use strict';
 
+/* 
 console.log(document.querySelector('.Start_guessing').textContent);
 document.querySelector('.Start_guessing').textContent = 'Correct Number';
+*/
+document.querySelector('.check_button').addEventListener('click', function () {
+  const guess_number = Number(document.querySelector('.guess_number').value);
+  console.log(guess_number, typeof guess_number);
+
+  if (!guess_number) {
+    document.querySelector('.Start_guessing').textContent = 'no Number.';
+  }
+});
