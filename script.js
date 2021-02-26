d1553fd2dcab69b547e2d37244a6da476e9c8365
Menos  Velasco jr <menozvelasco1@gmail.com>
2021-02-25 16:35:58 -0800

    add score play has lost the game

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ console.log(document.querySelector('.Start_guessing').textContent);
 document.querySelector('.Start_guessing').textContent = 'Correct Number';
 */
 const random_number = Math.trunc(Math.random() * 10) + 1;
+
+let score = 10;
+
 document.querySelector('.corrent_number').textContent = random_number;
 
 document.querySelector('.check_button').addEventListener('click', function () {
@@ -16,8 +19,24 @@ document.querySelector('.check_button').addEventListener('click', function () {
   } else if (guess_number === random_number) {
     document.querySelector('.Start_guessing').textContent = 'Correct Number';
   } else if (guess_number > random_number) {
-    document.querySelector('.Start_guessing').textContent = 'Too high';
+    if (score > 1) {
+      document.querySelector('.Start_guessing').textContent = 'Too high';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.Start_guessing').textContent =
+        'You lost the game.😂';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess_number < random_number) {
-    document.querySelector('.Start_guessing').textContent = 'Too low.';
+    if (score > 1) {
+      document.querySelector('.Start_guessing').textContent = 'Too low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.Start_guessing').textContent =
+        'You lost the game.😂';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
